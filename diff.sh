5cdb44adc96c2587c9b301e17c8aad145601b141
mapsam <matthews.sam@gmail.com>
2017-06-22 11:01:39 -0700

    add break in between images

--- diff.sh
+++ diff.sh
@@ -7,5 +7,5 @@ do
   filename=$(basename "$file")
   compare flags/v3.0.x/$filename flags/svg-strict-parsing/$filename flags/diff/$filename
   compare -compose src flags/v3.0.x/$filename flags/svg-strict-parsing/$filename flags/diff/$filename
-  convert flags/v3.0.x/$filename flags/svg-strict-parsing/$filename flags/diff/$filename +append flags/stitch/$filename
+  convert flags/v3.0.x/$filename break.png flags/svg-strict-parsing/$filename break.png flags/diff/$filename +append flags/stitch/$filename
 done
